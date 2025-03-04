c4b3ef649c426d893ed6318de53e511ae94cb8d8
RizkyPratama <rapratama2005@gmail.com>
2025-03-03 18:31:19 -0600

    made clean

--- SIM_TRACE/build/S_source.d
+++
@@ -1,5 +0,0 @@
-build/S_source.o: build/S_source.cpp build/../S_source.hh \
- models/ball/include/ball_numeric.h models/ball/include/ball.h
-build/../S_source.hh:
-models/ball/include/ball_numeric.h:
-models/ball/include/ball.h:
--- SIM_TRACE/build/home/rizky-ubuntu/trick_sims/TRACE/SIM_TRACE/models/ball/src/robot_numeric.d
+++
@@ -1,6 +0,0 @@
-build/home/rizky-ubuntu/trick_sims/TRACE/SIM_TRACE/models/ball/src/robot_numeric.o: \
- /home/rizky-ubuntu/trick_sims/TRACE/SIM_TRACE/models/ball/src/robot_numeric.c \
- /home/rizky-ubuntu/trick_sims/TRACE/SIM_TRACE/models/ball/src/../include/ball_numeric.h \
- /home/rizky-ubuntu/trick_sims/TRACE/SIM_TRACE/models/ball/src/../include/ball.h
-/home/rizky-ubuntu/trick_sims/TRACE/SIM_TRACE/models/ball/src/../include/ball_numeric.h:
-/home/rizky-ubuntu/trick_sims/TRACE/SIM_TRACE/models/ball/src/../include/ball.h:
