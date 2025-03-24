a14254e1f532040deb1a93e36de8b9e554b8202a
An Huynh <ahuynh750@gmail.com>
2025-03-24 18:50:15 -0500

    not working implementation, moved acc logic to robot

--- SIM_TRACE/models/ball/src/robot_numeric.c
+++ SIM_TRACE/models/ball/src/robot_numeric.c
@@ -10,6 +10,40 @@ int robot_deriv(BALL* B){
     double MOTORSPEED =.1;
     B->tiltrate[0] = MOTORSPEED * B->motor[0];
     B->tiltrate[1] = MOTORSPEED * B->motor[1];
+
+    // slide in the rX / rY calculations here!
+
+    B->prevX = B->errorX;
+    B->prevY = B->errorY;
+
+    B->errorX = B->targetX - B->pos[0];
+    B->errorY = B->targetY - B->pos[1];
+
+    B->integX = B->integX + B->errorX;
+    B->integY = B->integY + B->errorY;
+
+    B->derivX = B->errorX - B->prevX;
+    B->derivY = B->errorY - B->prevY;
+
+    double rX = B->errorX * B->kP + B->integX * B->kI + B->derivX * B->kD;
+    double rY = B->errorY * B->kP + B->integY * B->kI + B->derivY * B->kD;
+
+    if (rX > 30) {
+        rX = 30;
+    }
+    else if (rX < -30) {
+        rX = -30;
+    }
+    if (rY > 30) {
+        rY = 30;
+    }
+    else if (rY < -30) {
+        rY = -30;
+    }
+
+    B->acc[0] = rX;
+    B->acc[1] = rY;
+
     return (0) ;    
 }
 
